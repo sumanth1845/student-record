8fbf9ae020538a4da0ca177640e91f13b287e444
sumanth1845 <sumanth1845@gmail.com>
2026-04-30 16:56:06 +0530

    Update API_BASE URL for backend service

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,2 +1,2 @@
 // Backend API URL — Azure Web App
-const API_BASE = "https://studentrecord-webapp-backend-dpcjafc5cxbebpbz.centralus-01.azurewebsites.net";
+const API_BASE = "https://webapp1845-eab2cpbjepbthyg7.eastasia-01.azurewebsites.net";
